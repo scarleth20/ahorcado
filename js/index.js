fd2da269947e4ddeaaaa99b2ab2e1e82fb761fe1
César Bolaños <cesar.bolanos.andino@gmail.com>
2019-08-17 11:44:37 -0600

    Remove unused files

--- js/index.js
+++ js/index.js
@@ -1,36 +1,22 @@
-var config = {
+const config = {
   type: Phaser.AUTO,
+  parent: 'container',
   width: 800,
   height: 600,
-  physics:{
-    default: 'arcade',
-    arcade: {
-      gravity: {y: 300},
-      debug: false
-    }
-  },
   scene: {
     preload: preload,
-    create : create,
-    update : update
+    create : create
   }
 };
 
-var player ;
-var stars;
-var platforms;
-var cursors;
-var game = new Phaser.Game(config);
+const game = new Phaser.Game(config);
 
-  function preload() {
-    this.load.image('sky','assets/cielo.png');
-  }
+function preload() {
+  this.load.image('sky', 'assets/sky.png');
+}
 
- function create() {
-   const background = this.add.image(400, 300, 'sky');
-   background.displayWidth = game.config.width;
-   background.displayHeight = game.config.height;
- }
-
-  function update() {
-  }
+function create() {
+  const background = this.add.image(400, 300, 'sky');
+  background.displayWidth = game.config.width;
+  background.displayHeight = game.config.height;
+}
