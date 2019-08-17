bc99eb52641df2020d20b8151a3adf7f10378ad1
César Bolaños <cesar.bolanos.andino@gmail.com>
2019-08-17 12:07:25 -0600

    Add character boxes

--- js/index.js
+++ js/index.js
@@ -1,13 +1,15 @@
 const config = {
   type: Phaser.AUTO,
   parent: 'container',
-  width: 800,
+  width: 1000,
   height: 600,
   scene: {
     preload: preload,
     create : create
   }
 };
+
+const characters = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
 
 const game = new Phaser.Game(config);
 
@@ -16,7 +18,29 @@ function preload() {
 }
 
 function create() {
-  const background = this.add.image(400, 300, 'sky');
+  // Create background image
+  const background = this.add.image(500, 300, 'sky');
   background.displayWidth = game.config.width;
   background.displayHeight = game.config.height;
+
+  // Create graphics
+  const graphics = this.add.graphics();
+
+  // Build rectangles
+  buildRectangles(this, graphics);
+}
+
+function buildRectangles(self, graphics) {
+  let x = 50;
+
+  characters.split('').forEach((character) => {
+    graphics.lineStyle(5, 0xFF00FF, 1.0);
+    graphics.fillStyle(0xFFFFFF, 1.0);
+    graphics.fillRect(x, 500, 40, 40);
+    graphics.strokeRect(x, 500, 40, 40);
+
+    self.add.text(x + 12, 510, character, { color: '#000000', fontFamily: 'Arial', fontSize: 20, });
+
+    x += 50;
+  });
 }
