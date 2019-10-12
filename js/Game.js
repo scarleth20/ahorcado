3a7b34cdec7c24a87f9e55203f7064189a448ffc
scarleth <trujilloscarleth60@gmail.com>
2019-10-12 12:06:41 -0600

    hicimos aparecer el muñequito jaja

--- js/Game.js
+++ js/Game.js
@@ -12,6 +12,16 @@ class Game extends Phaser.Scene {
     this.load.image('head', 'assets/toy/head.png');
 
     this.load.image('body', 'assets/toy/body.png');
+
+    this.load.image('left-leg', 'assets/toy/left-leg.png');
+
+    this.load.image('right-leg', 'assets/toy/right-leg.png');
+
+    this.load.image('right-arm', 'assets/toy/right-arm.png');
+
+    this.load.image('left-arm', 'assets/toy/left-arm.png');
+
+    this.load.image('platform', 'assets/platform.png');
   }
 
 create() {
@@ -20,11 +30,21 @@ create() {
     sky.displayWidth = this.game.config.width;
     sky.displayHeight = this.game.config.height;
 
-    const head = this.add.image(450, 250, 'head');
+    const head = this.add.image(800, 140, 'head');
 
-    const body = this.add.image(450, 350, 'body');
+    const body =  this.add.image(800, 218, 'body');
 
-  // Create graphics
+    const leftLeg =  this.add.image(822, 315, 'left-leg');
+
+    const rightLeg =  this.add.image(780, 315, 'right-leg');
+
+    const rightArm =  this.add.image(770, 220, 'right-arm');
+
+    const leftArm =  this.add.image(829, 220, 'left-arm');
+
+    const platform =  this.add.image(700, 210, 'platform');
+
+ // Create graphics
      const graphics = this.add.graphics();
 
   // Build rectangles
